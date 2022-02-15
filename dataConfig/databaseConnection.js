157b477d3ba343758d605a5576d56283eafb6547
Nikhil Dev A S <asdevnikhil@gmail.com>
2022-02-15 15:30:06 +0530

    Database migrated to atlas

--- dataConfig/databaseConnection.js
+++ dataConfig/databaseConnection.js
@@ -4,7 +4,8 @@ const state ={
 }
 
 module.exports.connect = (callback)=>{
-    const url = 'mongodb://localhost:27017'
+    // const url = 'mongodb://localhost:27017'
+    const url = 'mongodb+srv://nikhil:nikhil123@cluster0.r8ds8.mongodb.net/footprint?retryWrites=true&w=majority'
     const dbname = 'FootPrint'
 
     MongodbClient.connect(url, {useUnifiedTopology: true}, (err, data)=>{
@@ -14,7 +15,6 @@ module.exports.connect = (callback)=>{
         state.db = data.db(dbname)
         callback()
     })
-
 }
 
 module.exports.get = ()=>{
